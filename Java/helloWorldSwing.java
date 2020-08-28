d8d17b207520eb1c273f1234a14433d26408a6d5
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-08-29 00:03:22 +0600

    Size and close button action added

--- Java/helloWorldSwing.java
+++ Java/helloWorldSwing.java
@@ -2,7 +2,9 @@ import javax.swing.JFrame;
 public class helloWorldSwing{
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setTitle("Hello World Java GUI");     
+        frame.setTitle("Hello World Java GUI");  
+        frame.setSize(800, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
         frame.setVisible(true);
     }
 }
