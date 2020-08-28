dccab8c3024463a5d2b1b4d3ca77d6967e47f2ac
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-08-28 23:58:14 +0600

    Basic Hello world java swing code

---
+++ Java/helloWorldSwing.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+public class helloWorldSwing{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Hello World Java GUI");     
+        frame.setVisible(true);
+    }
+}
