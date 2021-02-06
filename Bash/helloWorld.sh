73d26ffded9ee8c75e89b791231ffb4859ebdb78
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-02-07 01:38:40 +0600

    Add Hello 🗺 of Bash scripting

---
+++ Bash/helloWorld.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+helloWorld="Hello world of bash !"    #Declaring a variable in bash (Remeber that no space allowed between the equal sign)
+echo "$helloWorld"   #Printing the variable value
