c73b0e8e7c1769dae81227a95c1113cba76764e5
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-10-05 15:22:06 +0600

    Start C# learning
    Why:
    
    How:
    
    What:
    
    Tags:

---
+++ C#/helloWorld.cs
@@ -0,0 +1,14 @@
+// Hello world C# program by Mushfiqur Rahman Abir
+
+namespace Hello
+{
+    class World
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine
+                ("Hello World Of C#");
+            System.Console.ReadKey();
+        }
+    }
+}
